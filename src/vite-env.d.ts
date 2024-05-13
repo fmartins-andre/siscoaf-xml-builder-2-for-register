8062470b539efc0977ab81c017f33f6b841b0cf6
fmartins-andre <fmartins.andre@gmail.com>
2024-05-13 18:28:35 -0300

    chore: add typing to env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly DB_HOST: string;
+  readonly DB_PORT: number;
+  readonly DB_USER: string;
+  readonly DB_PASSWORD: string;
+  readonly DB_DATABASE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  process.env = { ...process.env, ...env }; // required to express as it uses process.env
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
