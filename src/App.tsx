fe3b9ce23e65ab468dbb7672fe4fdb63b79bd0d7
fmartins-andre <fmartins.andre@gmail.com>
2024-05-13 20:23:28 -0300

    feat: add sonner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "@/globals.css";
 import { FormSiscoaf } from "@/features/form-siscoaf";
 import { QueryClientProvider } from "@/config/providers/query-client.provider";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
     <QueryClientProvider>
       <FormSiscoaf />
+      <Toaster position="top-right" richColors />
     </QueryClientProvider>
   );
 }
